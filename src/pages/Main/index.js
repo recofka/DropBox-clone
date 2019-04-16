505cf77cc420553e39c66bf0d1d0d5bc1292d0f8
recofka <recofka@gmail.com>
2019-04-16 11:20:17 +0200

    development

---
+++ src/pages/Main/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './styles.css';
+import logo from '../../assets/logo.svg'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div id='main-container'>
+                <form>
+                    
+                    <i class="fab fa-dropbox fa-4x">CloneBox</i>
+                    <input placeholder='Create a box'/>
+                    <button type='submit'>Create</button>
+
+                </form>
+
+            </div>
+        );
+    }
+}
