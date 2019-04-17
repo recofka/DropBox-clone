048dfef3d6995f14d58dcbd41b1e427b27c31687
recofka <recofka@gmail.com>
2019-04-17 14:34:49 +0200

    Fetching box s information from API and showing title

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import { MdInsertDriveFile } from 'react-icons/md';
 import './styles.css';
+import api from '../../services/api';
 
 export default class Box extends Component {
+  state={
+    box: {}
+  }
+
+
+  async componentDidMount(){
+    const box = this.props.match.params.id;
+    const response = await api.get(`boxes/${box}`);
+    this.setState({ box: response.data})
+    console.log('response',response)
+  }
+
+
   render() {
-    return <div />;
+    return (
+      <div id='box-container'>
+        <header>
+          <i className="fab fa-dropbox fa-4x">CloneBox</i>
+          <h1>{this.state.box.title}</h1>
+        </header>
+        <ul>
+          <li>
+            <a className='fileInfo' href='oi'>
+            <MdInsertDriveFile size={24} color='#5c1f99' />
+            <strong>Nome do arquivo</strong>
+            </a>
+            <span>Ha 3 minutos atras</span>
+          </li>
+          <li>
+            <a className='fileInfo' href='oi'>
+            <MdInsertDriveFile size={24} color='#5c1f99' />
+            <strong>Nome do arquivo2</strong>
+            </a>
+            <span>Ha 3 minutos atras</span>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
