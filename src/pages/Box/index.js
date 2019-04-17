ab861032a50e6753422133de368a2dc0fb2342c0
recofka <recofka@gmail.com>
2019-04-17 15:03:47 +0200

    Showing file s informations

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -4,20 +4,24 @@ import './styles.css';
 import api from '../../services/api';
 
 export default class Box extends Component {
-  state={
+  state = {
     box: {}
   }
 
 
-  async componentDidMount(){
+  async componentDidMount() {
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
-    this.setState({ box: response.data})
-    console.log('response',response)
+    this.setState({ box: response.data })
+    console.log('response', response)
   }
 
 
   render() {
+    if (this.state.box.files === undefined){
+      return <h1>Loading</h1>
+    }
+    
     return (
       <div id='box-container'>
         <header>
@@ -25,20 +29,16 @@ export default class Box extends Component {
           <h1>{this.state.box.title}</h1>
         </header>
         <ul>
-          <li>
-            <a className='fileInfo' href='oi'>
-            <MdInsertDriveFile size={24} color='#5c1f99' />
-            <strong>Nome do arquivo</strong>
-            </a>
-            <span>Ha 3 minutos atras</span>
-          </li>
-          <li>
-            <a className='fileInfo' href='oi'>
-            <MdInsertDriveFile size={24} color='#5c1f99' />
-            <strong>Nome do arquivo2</strong>
-            </a>
-            <span>Ha 3 minutos atras</span>
-          </li>
+       
+          {this.state.box.files && this.state.box.files.map(file => (
+            <li key={file._id}>
+              <a className='fileInfo' href={file.url} target='blank'>
+                <MdInsertDriveFile size={24} color='#5c1f99' />
+                <strong>{file.title}</strong>
+              </a>
+              <span>{file.createdAt}</span>
+            </li>
+          ))}
         </ul>
       </div>
     );
