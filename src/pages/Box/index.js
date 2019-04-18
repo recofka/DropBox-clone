2313fb5a90422a9b08407ac19db71f2b056df210
recofka <recofka@gmail.com>
2019-04-18 09:29:32 +0200

    Create feat to upload files using api

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { MdInsertDriveFile } from 'react-icons/md';
 import { distanceInWords } from 'date-fns';
+import Dropzone from 'react-dropzone';
 import './styles.css';
 import api from '../../services/api';
 
@@ -17,20 +18,38 @@ export default class Box extends Component {
     console.log('response', response)
   }
 
+  handleUpload = (files) => {
+    files.forEach(element => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+      
+      data.append('file', element);
+      api.post(`boxes/${box}/file`, data);
+    });
+  }
 
   render() {
-    if (this.state.box.files === undefined){
+    if (this.state.box.files === undefined) {
       return <h1 id='box-loading'>Loading</h1>
     }
-    
+
     return (
       <div id='box-container'>
         <header>
           <i className="fab fa-dropbox fa-4x">CloneBox</i>
           <h1>{this.state.box.title}</h1>
         </header>
+
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({getRootProps, getInputProps})=> (
+            <div className='upload' {...getRootProps()}>
+              <input {...getInputProps()}/>
+              <p>Drop to upload your files or choose your files</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
-       
           {this.state.box.files && this.state.box.files.map(file => (
             <li key={file._id}>
               <a className='fileInfo' href={file.url} target='blank'>
