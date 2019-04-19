e1115c536b3eff98f5321217254cd280b3105c36
recofka <recofka@gmail.com>
2019-04-19 10:24:55 +0200

    Used eslint to normalize code

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
+
 const routes = express.Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FilleController');
 
-routes.get('/', (req, res) => {
-    return res.send("basic server is running")
-});
+routes.get('/', (req, res) => res.send('basic server is running'));
 
 routes.post('/boxes', BoxController.store);
 routes.get('/boxes/:id', BoxController.show);
