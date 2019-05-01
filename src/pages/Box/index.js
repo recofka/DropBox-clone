802b05593f78793531e70a541483a769e63a1f09
recofka <recofka@gmail.com>
2019-05-01 15:31:20 +0200

    Normalized code

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -8,22 +8,22 @@ import api from '../../services/api';
 
 export default class Box extends Component {
   state = {
-    box: {}
+    box: {},
   }
 
 
   async componentDidMount() {
-    //"subscribe" at socket.io
+    // "subscribe" at socket.io
     this.subscribeToNewFiles();
 
-    //Post files in the API
+    // Post files in the API
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
-    this.setState({ box: response.data })
+    this.setState({ box: response.data });
   }
 
   handleUpload = (files) => {
-    files.forEach(element => {
+    files.forEach((element) => {
       const data = new FormData();
       const box = this.props.match.params.id;
 
@@ -37,26 +37,28 @@ export default class Box extends Component {
     const io = socket('https://hidden-shore-25474.herokuapp.com');
 
     io.emit('connectRoom', box);
-    
-    io.on('file', data => {
-      this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files] } })
-    })
+
+    io.on('file', (data) => {
+      this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files] } });
+    });
   }
+
   render() {
-    if (this.state.box.files === undefined) {
-      return <h1 id='box-loading'>Loading</h1>
+    const { title, files } = this.state.box;
+    if (files === undefined) {
+      return (<h1 id="box-loading">Loading</h1>);
     }
 
     return (
-      <div id='box-container'>
+      <div id="box-container">
         <header>
           <i className="fab fa-dropbox fa-4x">CloneBox</i>
-          <h1>{this.state.box.title}</h1>
+          <h1>{title}</h1>
         </header>
 
         <Dropzone onDropAccepted={this.handleUpload}>
           {({ getRootProps, getInputProps }) => (
-            <div className='upload' {...getRootProps()}>
+            <div className="upload" {...getRootProps()}>
               <input {...getInputProps()} />
               <p>Drop to upload your files or choose your files</p>
             </div>
@@ -64,13 +66,18 @@ export default class Box extends Component {
         </Dropzone>
 
         <ul>
-          {this.state.box.files && this.state.box.files.map(file => (
+          {files && files.map(file => (
+            // eslint-disable-next-line no-underscore-dangle
             <li key={file._id}>
-              <a className='fileInfo' href={file.url} target='blank'>
-                <MdInsertDriveFile size={24} color='#7159c1' />
+              <a className="fileInfo" href={file.url} target="blank">
+                <MdInsertDriveFile size={24} color="#7159c1" />
                 <strong>{file.title}</strong>
               </a>
-              <span>{distanceInWords(file.createdAt, new Date())} ago</span>
+              <span>
+                {distanceInWords(file.createdAt, new Date())}
+                {' '}
+                ago
+              </span>
             </li>
           ))}
         </ul>
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import './styles.css';
 import api from '../../services/api';
@@ -5,49 +7,47 @@ import AllBoxes from '../AllBoxes/index';
 
 export default class Main extends Component {
     state = {
-        newBox: '',
-        allBoxes: '',
+      newBox: '',
+      allBoxes: '',
     };
 
     async componentDidMount() {
-      const response = await api.get(`allboxes`)
-      this.setState({ allBoxes: response.data })
+      const response = await api.get('allboxes');
+      this.setState({ allBoxes: response.data });
     }
 
-    handleSubmit = async event => {
-        event.preventDefault();
+    handleSubmit = async (event) => {
+      event.preventDefault();
 
-        const response = await api.post('boxes', {
-            title: this.state.newBox
-        });
-        this.props.history.push(`/box/${response.data._id}`)
+      const response = await api.post('boxes', {
+        title: this.state.newBox,
+      });
+      // eslint-disable-next-line no-underscore-dangle
+      this.props.history.push(`/box/${response.data._id}`);
     }
 
     handleInputChange = (event) => {
-        this.setState({ newBox: event.target.value })
+      this.setState({ newBox: event.target.value });
     }
 
     render() {
-        return (
-            <div>
-            <div id='main-container'>
-                <form onSubmit={this.handleSubmit}>
-                    <i className="fab fa-dropbox fa-4x">CloneBox</i>
-                    <input
-                        placeholder='Create a box'
-                        value={this.state.newBox}
-                        onChange={this.handleInputChange}
-                    />
-                    <button type='submit'>Create</button>
-
-                </form>
-                
-
-            </div>
-            <div>
-            <AllBoxes boxes={this.state.allBoxes}/>
+      return (
+        <div>
+          <div id="main-container">
+            <form onSubmit={this.handleSubmit}>
+              <i className="fab fa-dropbox fa-4x">CloneBox</i>
+              <input
+                placeholder="Create a box"
+                value={this.state.newBox}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit">Create</button>
+            </form>
+          </div>
+          <div>
+            <AllBoxes boxes={this.state.allBoxes} />
+          </div>
         </div>
-        </div>
-        );
+      );
     }
 }
